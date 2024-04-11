630624de22955a1146fa8dfa86d5e66889b82e88
Akash Agrawal <akashagrawal0358@gmail.com>
2024-04-11 22:35:44 +0530

    Car images some-showing successfully

--- client/components/CarCard.tsx
+++ client/components/CarCard.tsx
@@ -2,7 +2,7 @@
 import { CarProps } from "@/types";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { useState } from "react";
 import CarDetails from "./CarDetails";
 
@@ -37,7 +37,7 @@ const CarCard = ({ car }: AllCarProps) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
